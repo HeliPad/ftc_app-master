b80ae6e14e193d51fdf5edca5780ce42aae45e1f
Ben Madsen <bmadsen513@rsdmo.org>
2017-10-08 06:44:06 +0000

    Changed targetHeading from storing the quad. # to the actual heading

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivingMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivingMain.java
@@ -154,11 +154,11 @@ public class DrivingMain extends LinearOpMode {
     public void reOrient() {
         float curHeading = (float)robot.gyro.getHeading();
         //target heading is from 0-4 (0,90,180,270,360)
-        int targetHeading = (int)(curHeading/90 + 0.5);
+        int targetHeading = (int)(curHeading/90 + 0.5) * 90;
         
         //Turns robot towards Target Heading (added last part just in case Cur==Target @ 0)
         //Multiplied target heading by 90 to allow all cases to work (if curHeading>targetHeading && targetHeading!=0. then the robot would've moved CCW)
-        if ((mod(targetHeading*90 - curHeading + 180, 360) - 180) < 0) {//if(curHeading < (targetHeading == 0 ? 360 : targetHeading && curHeading != targetHeading)){ this will not always take the shortest route to the target
+        if ((mod(targetHeading - curHeading + 180, 360) - 180) < 0) {//if(curHeading < (targetHeading == 0 ? 360 : targetHeading && curHeading != targetHeading)){ this will not always take the shortest route to the target
                 robot.rightMotorB.setPower(.5); //test and set to power that'll ensure greatest accuracy:speed ratio
                 robot.rightMotorF.setPower(.5);
                 robot.leftMotorF.setPower(-.5);
